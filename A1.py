4e941f95045f3bc00713102cf126e0f791f58850
jonathanwesley1 <jonathan_wesley10@hotmail.com>
2021-09-25 15:19:18 -0300

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,27 +1,32 @@
 import random
 QTD_MINIMA_INTERACAO = 7
-
-def menu (opcao):
-    if(sala == 6):
-        return "Esta sala existe apenas um caminho:\n[1] Caminho preto"
-    return "Escolha seu caminho:\n[1] Caminho vermelho\n[2] Caminho preto"
-
 salaSorteada = [1,2,3,4,5]
 countInteracao = 0
 sala = 1
 
+def menu (opcao):
+    if(sala == 6):
+        return "Esta sala existe apenas um caminho: "
+    return "Escolha seu caminho:\n[1] Caminho vermelho\n[2] Caminho preto"
+
+def resultCaminho (sala):
+    if(caminho == 2 or sala == 6):
+        if(sala == 8):
+            return sala + 1
+        return sala + 2  
+    return sala + 1
+    
 while(countInteracao < QTD_MINIMA_INTERACAO and sala < 8):
     print("Você está na sala: ", sala)
-
-    print(menu(sala))
-
-    caminho = int(input())
-    countInteracao = countInteracao + 1
     
-    if(caminho == 2 or sala == 6):
-        sala = sala + 2
+    if(sala != 6):
+        print(menu(sala))
+        caminho = int(input())
+        sala = resultCaminho(sala)
+        countInteracao = countInteracao + 1
     else:
-        sala = sala + 1    
+        print(menu(sala))
+        sala = resultCaminho(sala)
 
 if(sala == 8 and countInteracao < QTD_MINIMA_INTERACAO):
     print("\nVocê está na sala: 8")
@@ -37,17 +42,17 @@ if(sala == 8 and countInteracao < QTD_MINIMA_INTERACAO):
     else:
         sala = (random.choice(salaSorteada)) 
    
-        while(countInteracao < QTD_MINIMA_INTERACAO):
+        while(countInteracao < QTD_MINIMA_INTERACAO and sala < 9):
             print("Você está na sala: ", sala)
-        
-            print(menu(sala))
-            caminho = int(input())
-            countInteracao = countInteracao + 1
-    
-            if(caminho == 2 or sala == 6):
-                 sala = sala + 2
+
+            if(sala != 6):
+                print(menu(sala))
+                caminho = int(input())
+                sala = resultCaminho(sala)
+                countInteracao = countInteracao + 1
             else:
-                 sala = sala + 1 
+                print(menu(sala))
+                sala = resultCaminho(sala)
 
 
 if(countInteracao < QTD_MINIMA_INTERACAO and sala == 9):
